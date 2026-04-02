e453d020fb0797c260390321d870cb46d49ce555
Huzam229 <huzamkhan99@gmail.com>
2026-04-02 17:42:55 +0500

    Email verification functionality.

--- app/api/auth/register/route.js
+++ app/api/auth/register/route.js
@@ -2,6 +2,8 @@ import connectDB from "@/lib/db";
 import UserModel from "@/models/user.model";
 import { z } from 'zod';
 import response from "@/lib/helperFunction";
+import * as jose from 'jose'
+
 
 export async function POST(req) {
     try {
@@ -26,6 +28,14 @@ export async function POST(req) {
             name, email, password
         })
         await newUser.save();
+
+        const secretKey = new TextEncoder().encode(process.env.JWT_SECRET);
+        const token = await new jose.SignJWT({ id: newUser._id }).setIssuedAt()
+            .setExpirationTime('1h')
+            .setProtectedHeader({ alg: 'HS256' })
+            .sign(secretKey);
+
+        return response(true, 200, 'User registered successfully', { token });
 
     } catch (error) {
         console.log(error);
