c4a22def76e670ecd9c7ed6085de2997a8840b7f
Huzam229 <huzamkhan99@gmail.com>
2026-04-08 16:20:54 +0500

    Move verify-email folder in the app foler and in verify-email foler create [token] folder because it have param in the url

---
+++ app/(root)/auth/verify-email/[token]/page.jsx
@@ -0,0 +1,10 @@
+import { use } from "react"
+const EmailVerification = ({ params }) => {
+    const { token } = use(params)
+    console.log(token)
+    return (
+        <div> EmailVerification</ div>
+    )
+}
+
+export default EmailVerification
