50e339f47f7440a91e9bcf445a441a8c667e275f
Huzam229 <huzamkhan99@gmail.com>
2026-04-14 18:20:51 +0500

    Change next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,6 @@
-import { defineConfig } from "next";
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  allowedDevOrigins: ["13.62.106.72"],
-
-  // 🚀 REQUIRED for EC2 production deployment
-  output: "standalone",
+  allowedDevOrigins: ['13.62.106.72'],
 };
 
 export default nextConfig;
