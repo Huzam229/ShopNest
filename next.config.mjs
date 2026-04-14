8078f4954f09f9bf5da2d5022ff3065e7fa43d26
Huzam229 <huzamkhan99@gmail.com>
2026-04-14 18:19:03 +0500

    Add new instance in the AWS

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,11 @@
+import { defineConfig } from "next";
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  allowedDevOrigins: ['13.62.106.72'],
+  allowedDevOrigins: ["13.62.106.72"],
+
+  // 🚀 REQUIRED for EC2 production deployment
+  output: "standalone",
 };
 
 export default nextConfig;
